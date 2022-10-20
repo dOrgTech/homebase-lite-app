390e5f9a0a2704cb84cc494ac7332a8ec42c78af
fabiolalombardim <fabiola@dorg.tech>
2022-10-20 17:33:59 +0200

    updating branch

--- src/modules/creator/index.tsx
+++ src/modules/creator/index.tsx
@@ -186,6 +186,8 @@ const CommunityForm = ({ submitForm, values, setFieldValue, errors, touched, set
 
   const { data: tokenMetadata, isLoading: loading, error } = useTokenMetadata(values?.tokenAddress)
 
+  console.log('test commit')
+
   useEffect(() => {
     if (tokenMetadata) {
       setFieldValue("tokenID", tokenMetadata.token_id)
