fd179fe7d5ae5f1a9beef58490dd2008cb23dc79
Manank Patni <manank321@gmail.com>
2022-10-30 01:52:44 +0530

    Add signature to the frontend before sending calls to backend
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,10 +1,12 @@
-
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import updateLocale from 'dayjs/plugin/updateLocale'
-import { Choice } from 'models/Choice';
+import { char2Bytes } from "@taquito/tzip16"
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+import updateLocale from "dayjs/plugin/updateLocale"
+import { Choice } from "models/Choice"
 import { networkNameMap } from "./bakingBad"
 import { Network } from "./beacon"
+import { BeaconWallet } from "@taquito/beacon-wallet"
+import { RequestSignPayloadInput, SigningType } from "@airgap/beacon-sdk"
 
 export const getCurrentBlock = async (network: Network) => {
   const url = `https://api.${networkNameMap[network]}.tzkt.io/v1/head`
@@ -32,7 +34,12 @@ export const getTotalSupplyAtReferenceBlock = async (network: Network, address: 
   return result[0].value
 }
 
-export const getUserTotalSupplyAtReferenceBlock = async (network: Network, address: string, level: number, userAddress: string) => {
+export const getUserTotalSupplyAtReferenceBlock = async (
+  network: Network,
+  address: string,
+  level: number,
+  userAddress: string
+) => {
   const url = `https://api.${networkNameMap[network]}.tzkt.io/v1/contracts/${address}/bigmaps/ledger/historical_keys/${level}`
   const response = await fetch(url)
 
@@ -42,13 +49,13 @@ export const getUserTotalSupplyAtReferenceBlock = async (network: Network, addre
 
   const result = await response.json()
 
-  let userBalance;
+  let userBalance
 
   if (result && result.length > 0) {
     userBalance = result.find((elem: any) => elem.key.address === userAddress)
-    return userBalance.value;
+    return userBalance.value
   }
-  return 0;
+  return 0
 }
 
 export const hasTokenBalance = async (network: Network, account: string, contract: any) => {
@@ -61,20 +68,19 @@ export const hasTokenBalance = async (network: Network, account: string, contrac
 
   const result = await response.json()
 
-  let hasBalance = false;
+  let hasBalance = false
 
   if (result && result[0]) {
     if (result[0].balance > 0) {
-      hasBalance = true;
+      hasBalance = true
     } else {
-      hasBalance = false;
+      hasBalance = false
     }
   } else {
-    hasBalance = false;
+    hasBalance = false
   }
 
-  return hasBalance;
-
+  return hasBalance
 }
 
 export const getTurnoutValue = async (network: Network, address: string, level: number, voters: number) => {
@@ -87,24 +93,23 @@ export const getTurnoutValue = async (network: Network, address: string, level: 
   const result = await response.json()
 
   if (result) {
-    return (voters * 100) / result.length;
+    return (voters * 100) / result.length
   }
 
-  return 0;
-
+  return 0
 }
 
 export const isProposalActive = (date: number) => {
   const config = {
     rounding: Math.floor
   }
-  dayjs.extend(relativeTime, config);
+  dayjs.extend(relativeTime, config)
   dayjs.extend(updateLocale)
-  dayjs.updateLocale('en', {
+  dayjs.updateLocale("en", {
     relativeTime: {
       future: "%s left",
       past: "%s ago",
-      s: 'a few seconds',
+      s: "a few seconds",
       m: "1 minute",
       mm: "%d minutes",
       h: "1 hour",
@@ -117,75 +122,101 @@ export const isProposalActive = (date: number) => {
       yy: "%d years"
     }
   })
-  const remainingDate = dayjs(date).fromNow();
-  return remainingDate;
+  const remainingDate = dayjs(date).fromNow()
+  return remainingDate
 }
 
 export const calculateWeight = (totalSupply: string, balance: string, decimals: any) => {
-  const formattedTotalSupply = Number(totalSupply) / (Number(decimals) ** 10)
-  return (Number(balance) * 100) /formattedTotalSupply
+  const formattedTotalSupply = Number(totalSupply) / Number(decimals) ** 10
+  return (Number(balance) * 100) / formattedTotalSupply
 }
 
 export const calculateChoiceTotal = (choices: any[], decimals: any) => {
-  let total = 0;
+  let total = 0
   choices.map((choice: any) => {
     total += Number(choice.balanceAtReferenceBlock)
   })
-  const result = total / (Number(decimals) ** 10)
+  const result = total / Number(decimals) ** 10
 
-  return result;
+  return result
 }
 
 export const calculateProposalTotal = (choices: Choice[], decimals: any) => {
-  let total = 0;
+  let total = 0
   choices.map((choice: any) => {
     choice.walletAddresses.map((elem: any) => {
       total += Number(elem.balanceAtReferenceBlock)
     })
   })
-  const result = total / (Number(decimals) ** 10)
+  const result = total / Number(decimals) ** 10
 
-  return result;
+  return result
 }
 
 export const getTotalVoters = (choices: Choice[]) => {
-  let votersTotal = 0;
+  let votersTotal = 0
   choices.map((choice: Choice) => {
-    votersTotal += choice.walletAddresses.length;
+    votersTotal += choice.walletAddresses.length
   })
-  return votersTotal;
+  return votersTotal
 }
 
 export const getTreasuryPercentage = (proposalTotal: number, totalSupply: number) => {
   const value = (Number(proposalTotal) * 100) / Number(totalSupply)
 
-  return value;
+  return value
 }
 
 export const numberWithCommas = (x: number) => {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
-const SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"];
+const SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"]
 
 export const nFormatter = (num: number, digits: number) => {
   // what tier? (determines SI symbol)
-  const tier = Math.log10(Math.abs(num)) / 3 | 0;
+  const tier = (Math.log10(Math.abs(num)) / 3) | 0
 
   // if zero, we don't need a suffix
-  if (tier == 0) return num.toFixed(1);
+  if (tier == 0) return num.toFixed(1)
 
   // get suffix and determine scale
-  const suffix = SI_SYMBOL[tier];
-  const scale = Math.pow(10, tier * 3);
+  const suffix = SI_SYMBOL[tier]
+  const scale = Math.pow(10, tier * 3)
 
   // scale the number
-  const scaled = num / scale;
+  const scaled = num / scale
 
   // format number and add suffix
-  return scaled.toFixed(1) + suffix;
+  return scaled.toFixed(1) + suffix
 }
 
 export const formatByDecimals = (value: string, decimals: string) => {
-  return   nFormatter(Number(value) / (Number(decimals) ** 10), 1)
+  return nFormatter(Number(value) / Number(decimals) ** 10, 1)
+}
+
+export const getSignature = async (userAddress: string, wallet: BeaconWallet | undefined) => {
+  if (!wallet) {
+    return
+  }
+  const formattedInput: string = [
+    "Tezos Signed Message:",
+    // "http://localhost:3000",
+    // new Date().toISOString(),
+    `Tezos Homebase Lite - I am ${userAddress}`
+  ].join(" ")
+
+  const bytes = char2Bytes(formattedInput)
+  const payloadBytes = "05" + "0100" + char2Bytes(bytes.length.toString()) + bytes
+
+  const payload: RequestSignPayloadInput = {
+    signingType: SigningType.MICHELINE,
+    payload: payloadBytes,
+    sourceAddress: userAddress
+  }
+
+  const signedPayload = await wallet.client.requestSignPayload(payload)
+  const { signature } = signedPayload
+
+  return signature
 }
